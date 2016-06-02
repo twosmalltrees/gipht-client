50f8a0f3bcb092394c2df7c3570391660b227c59
twosmalltrees <harrison@twosmalltrees.com>
2016-06-02 12:39:00 +1000

    jtoker set up

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+import $ from'jquery';
+import 'j-toker';
+
+// Configure auth with j-toker
+$.auth.configure({
+  apiUrl: 'http://localhost:3000'
+});
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('.container')
+);
